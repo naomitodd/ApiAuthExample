a733d51f2e12b0cd1b4476b156b5a993f81e6041
Naomi Todd <Naomi.TODD@education.gov.uk>
2023-11-03 13:35:41 +0000

    Add swagger support for API Key

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-using ApiAuthExample;
 using ApiAuthExample.Authentication;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +12,34 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Description = "The API Key to access to API",
+        Type = SecuritySchemeType.ApiKey,
+        Name = "x-api-key",
+        In = ParameterLocation.Header,
+        Scheme = "ApiKeyScheme"
+    });
+
+    var scheme = new OpenApiSecurityScheme
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header,
+    };
+
+    var requirement = new OpenApiSecurityRequirement
+    {
+        {scheme, new List<string>() }
+    };
+
+    c.AddSecurityRequirement(requirement);
+});
 
 //scoped filter - added as ServiceFilter attribute
 builder.Services.AddScoped<ApiKeyAuthFilter>();
